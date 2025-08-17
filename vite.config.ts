2ce8f34a2cf26b76cad88207b345611bdcc79167
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2025-08-17 21:35:39 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        research: resolve(__dirname, 'research.html'),
+      },
+    },
   },
 });
